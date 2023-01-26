650f526e10ea5783b6918d4f9ce4fb05d54064d5
Иван Чепрасов <chandy.ru@gmail.com>
2023-01-26 07:44:38 +0700

    Добавил заголовочный файл

---
+++ KURSOVAYA/Header.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const int N = 10; //Размерность поля
--- KURSOVAYA/KURSOVAYA.cpp
+++ KURSOVAYA/KURSOVAYA.cpp
@@ -2,11 +2,17 @@
 //
 
 #include <iostream>
+#include "Header.h"
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //type POLE;
+    cout << "Hello World!\n";
 }
+
+ 
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
