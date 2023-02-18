75e85c77f4dba706cd521ab458b0594de6a31cb2
Иван Чепрасов <chandy.ru@gmail.com>
2023-02-18 10:09:47 +0700

    Сделали неработающий выстрел)но сложный)

--- KURSOVAYA/Igrok.cpp
+++ KURSOVAYA/Igrok.cpp
@@ -1,7 +1,19 @@
 #include "Igrok.h"
-#include "pole.h"
+
+
 Igrok::Igrok(string imya)
 {
+	fdobit = var1; //Нет корабля который нужно добивать
+	napr = 0;  //Нет направления по которому стреляем
+	ch = 0;
+	xnach = -1; 
+	ynach = -1;
+	xpre = -1;
+	ypre = -1;
+	naprmimo[0] = 0;
+	naprmimo[1] = 0;
+	naprmimo[2] = 0;
+	naprmimo[3] = 0;
 	this->imya = imya;
 	int i,j;
 	for (i = 0; i < N; i++) //Инициализация массива сделанных выстрелов
@@ -13,41 +25,191 @@ Igrok::Igrok(string imya)
 	}
 };
 
-void Igrok::vystrel(int*x, int*y, pole* doska) //Генерация поля в которое игрок стреляет
-{   //Не завершено нужно вынести локальные переменные в класс
-	int xnach, ynach, xk[4], yk[4], napr,fdobit;
-	sost resposxoda;
-
-	if (doska->resposlxoda == "Попал")
+void Igrok::vystrel(int* x, int* y, pole* doska) //Генерация поля в которое игрок стреляет
+{   
+	
+	if ((doska->resposlxoda == "Попал") && (fdobit == var1)) //Первое попадание
 	{
-		fdobit = 1;
-		
+		fdobit = var2;
+	}
+
+	if ((doska->resposlxoda == "Попал") && (fdobit == var2)) //Второе попадание
+	{
+		fdobit = var3;
+	}
+
+	if ((doska->resposlxoda == "Попал") && (fdobit == var3)) //
+	{
+		fdobit = var3;
+	}
+
+	if ((doska->resposlxoda == "Попал") && ( fdobit == var3A)) //
+	{
+		fdobit = var3;
+	}
+
+	if (((doska->resposlxoda == "Мимо") || (doska->resposlxoda == "Тоже мимо")) && (fdobit == var2)) //Промах после первого попадания
+	{
+		fdobit = var3A;
+	}
+
+	if (((doska->resposlxoda == "Мимо") || (doska->resposlxoda == "Тоже мимо")) && (fdobit == var3)) //Промах после серии попаданий
+	{
+		fdobit = var4;
+	}
+
+	if (doska->resposlxoda == "Убит") //Если убили корабль
+	{
+		fdobit = var5;
 	}
 	
-	if (fdobit == 1) //Пока флажок стоит надо что-то делать
+	if (fdobit == var5)
 	{
-		/*//Выбираем направление
-		napr = (int)((rand() / 32767.0) * (3)) + 1; //Выбераем одно из 4-х направлений
-		//Должно быть сложнее
+		//Заново инициализируем переменные
+		fdobit = var1; //Нет корабля который нужно добивать
+		napr = 0;  //Нет направления по которому стреляем
+		ch = 0;
+		xnach = -1;
+		ynach = -1;
+		xpre = -1;
+		ypre = -1;
+		naprmimo[0] = 0;
+		naprmimo[1] = 0;
+		naprmimo[2] = 0;
+		naprmimo[3] = 0;
+
+		//Повторяем действия варианта 1
+		do
+		{
+			*(x) = (int)((rand() / 32767.0) * (N));
+			*(y) = (int)((rand() / 32767.0) * (N));
+		} while (masproverky[*(x)][*(y)] == PUSTPOD);
+
+		masproverky[*(x)][*(y)] = PUSTPOD;
+		//Хранят результата последнего хода
+		xnach = *(x);
+		ynach = *(y);
+	}
+
+	if (fdobit == var4)
+	{
 		if (napr == 1)
 		{
-			xk = xnach + 1;
+			napr = 2;
+			xpre = xnach + 1;
 		}
 		if (napr == 2)
 		{
-			xk = xnach - 1;
+			napr = 1;
+			xpre = xnach - 1;
 		}
 		if (napr == 3)
 		{
-			yk = ynach + 1;
+			napr = 4;
+			ypre = ynach + 1;
 		}
 		if (napr == 4)
 		{
-			yk = ynach - 1;
+			napr = 3;
+			ypre = ynach - 1;
 		}
-		*/
+
+		*(x) = xpre;
+		*(y) = ypre;
 	}
-	else
+
+	if (fdobit == var3A)
+	{
+		//Выбираем направление
+		do {
+			napr = (int)((rand() / 32767.0) * (3)) + 1; //Выбераем одно из 4-х направлений
+
+			if ((napr == naprmimo[0]) || (napr == naprmimo[1]) || (napr == naprmimo[2]))
+			{
+				continue;
+			}
+			else
+			{
+				ch = ch + 1;
+				naprmimo[ch] = napr;
+				break;
+			}
+		}
+		while (1);
+
+		if (napr == 1)
+		{
+			xpre = xnach + 1;
+		}
+		if (napr == 2)
+		{
+			xpre = xnach - 1;
+		}
+		if (napr == 3)
+		{
+			ypre = ynach + 1;
+		}
+		if (napr == 4)
+		{
+			ypre = ynach - 1;
+		}
+
+		*(x) = xpre;
+		*(y) = ypre;
+	}
+
+	if (fdobit == var3)
+	{
+		if (napr == 1)
+		{
+			xpre = xpre + 1;
+		}
+		if (napr == 2)
+		{
+			xpre = xpre - 1;
+		}
+		if (napr == 3)
+		{
+			ypre = ypre + 1;
+		}
+		if (napr == 4)
+		{
+			ypre = ypre - 1;
+		}
+
+		*(x) = xpre;
+		*(y) = ypre;
+	}
+
+	if (fdobit == var2) 
+	{
+		//Выбираем направление
+		napr = (int)((rand() / 32767.0) * (3)) + 1; //Выбераем одно из 4-х направлений
+		naprmimo[ch] = napr;
+
+		if (napr == 1)
+		{
+			xpre = xnach + 1;
+		}
+		if (napr == 2)
+		{
+			xpre = xnach - 1;
+		}
+		if (napr == 3)
+		{
+			ypre = ynach + 1;
+		}
+		if (napr == 4)
+		{
+			ypre = ynach - 1;
+		}
+
+		*(x) = xpre;
+		*(y) = ypre;
+	}
+
+	
+	if (fdobit == var1)
 	{
 		do 
 		{
--- KURSOVAYA/Igrok.h
+++ KURSOVAYA/Igrok.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "Header.h"
+#include "pole.h"
 
 class Igrok
 {
 private:
 	string imya;
 	sostoyanie masproverky[N][N]; //Хранит сделанные выстрелы игрока
+	varianti fdobit; //Флажок для функции выстрел
+	int napr; //Направление выстрелов по подбитому кораблю
+	int xnach, ynach, xpre, ypre, naprmimo[4], ch; //Переменные для алгоритма добития корабля
 public:
 	Igrok(string imya);
 	void vystrel(int* x, int* y, pole* doska);
